b34b499b6a795fdc493ed9b88184db5b6ff6468d
Meet Tarpara <meettarapara1@gmail.com>
2024-08-01 19:27:39 +0530

    create pages and rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import React from 'react'
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return (
+  return (5y m,kuu
     <>
-    <div className='text-teal-900'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/sign-in' element={<Signin/>}></Route>
+        <Route path='/sign-up' element={<SignUp/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+
+      </Routes>
+    </BrowserRouter>
     </>
     
   )
 }
 
+5
