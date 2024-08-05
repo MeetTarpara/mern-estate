09ba53ae959dc5918563bbd649b418deb7a012cc
Meet Tarpara <meettarapara1@gmail.com>
2024-08-05 19:32:08 +0530

    Create SignIn Api Route

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user_model.js";
 import bcrypt from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res,next) => {
    
@@ -33,3 +34,51 @@ export const test1 = (req , res) => {
         message : "Hello World!!"
     })
 };
+
+// export const signin = async (req, res, next) => {
+//     //data came from body
+//     const { email, password } = req.body;
+
+//     try {
+//              //find user by email
+//         const validUser = await User.findOne({ email }); 
+//         //check if user exists
+//         if (!validUser) { 
+//             return next(errorHandler(404,"User Not Found"));
+//         }  
+//         //check if password is correct
+//         const isMatch = bcrypt.compareSync(password, validUser.password);
+//         if (!isMatch) {
+//             return next(errorHandler(404,"Wrong Password"));
+//         }
+//         //generate and send token
+//         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+//         res
+//         .cookie('access_token', token , {httpOnly : true})
+//         .status(200)
+//         .json(validUser)
+        
+//     } catch (error) {
+//         next(error);
+//     }
+
+// }
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return next(errorHandler(404, 'User not found!'));
+      const validPassword = bcrypt.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+      //for ignor password in responce
+      const { password: pass, ...rest } = validUser._doc;
+      res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
--- api/routes/auth_route.js
+++ api/routes/auth_route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { signup, test1 } from '../controllers/auth_controller.js';
+import { signin, signup, test1 } from '../controllers/auth_controller.js';
 
 const router = express.Router();
 
 
 router.get("/signup",test1);
 router.post("/signup",signup);
+router.post("/signin",signin);
 
 
 
